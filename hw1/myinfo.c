146ede064963cd93e502356454fcf873f6737092
Krishna Madhuri <kp0042@uah.edu>
2015-05-30 19:03:28 -0500

    Checkin Assignment One

---
+++ hw1/myinfo.c
@@ -0,0 +1,34 @@
+/*****************************************************
+* Title: Assignment One CS590
+* Date : 05/30/2015
+* Krishna Madhuri Paramkusem
+*****************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+int main()
+{
+uid_t uid;
+gid_t gid;
+struct passwd *mypasswd;
+char *path, *home;
+char hostname[100];
+uid = getuid();
+gid = getgid();
+mypasswd = getpwuid(uid);
+gethostname(hostname,sizeof(hostname));
+printf("---------------------------\n");
+printf("System name: %s\n", hostname);
+printf("User information:\n");
+printf("full name: %s\n", mypasswd->pw_gecos);
+printf("log name: %s\n", mypasswd->pw_name);
+printf("uid: %d\n", mypasswd->pw_uid);
+printf("gid: %d\n", mypasswd->pw_gid);
+printf("HOME dir: %s\n", mypasswd->pw_dir);
+printf("Default shell: %s\n",mypasswd->pw_shell);
+printf("-------------------------------\n");
+system("date");
+return 0;
+}
